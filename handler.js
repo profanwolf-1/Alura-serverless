2bb9f8b05a003ff5e241e8aff81126516e691cb7
GuilhermeBarroso-sys <guibarrosodeoliveira5@gmail.com>
2022-02-18 17:57:31 -0300

    creating endpoints

--- handler.js
+++ handler.js
@@ -3,7 +3,7 @@ let pacientes = []
 for(let p = 0; p < 50; p++) {
   pacientes.push({
     id: p,
-    nome: `Fulano${p}}`,
+    nome: `Fulano${p}`,
     idade: 20+p
   })
 }
@@ -16,19 +16,33 @@ function filtrarMaiorIdade(idade) {
   return pacientes.filter(({idade: _idade}) => _idade > idade)
 }
 module.exports.listarPacientes = async (event) => {
-  const {idade} = event
-  const paciente = filtrarMaiorIdade(idade);
+  // const {idade} = event
+  // const paciente = filtrarMaiorIdade(idade);
   return {
-    statusCode: 200,
+    statusCode: 200, 
     body: JSON.stringify(
       {
-       paciente 
+       pacientes
       },
       null,
       2
     ),
   };
-
-  // Use this code if you don't use the http event with the LAMBDA-PROXY integration
-  // return { message: 'Go Serverless v1.0! Your function executed successfully!', event };
 };
+
+module.exports.obterPaciente = async (event) => {
+  const {id} = event.pathParameters;
+  const paciente = pacientes.find(({id: _id}) => id == _id);
+  return {
+    statusCode: paciente ? 200 : 404, 
+    body: JSON.stringify(
+      {
+        response: paciente ? {paciente} : {error: "Nenhum paciente encontrado" }
+
+      },
+      null,
+      2
+    ),
+  };
+}
+
