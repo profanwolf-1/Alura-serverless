f36ef17db271118074ea3ee4efc5914d31a74a4b
GuilhermeBarroso-sys <guibarrosodeoliveira5@gmail.com>
2022-02-21 14:08:31 -0300

    Integrando projeto com dynamoDB

--- handler.js
+++ handler.js
@@ -1,6 +1,6 @@
 'use strict';
-let pacientes = []
-for(let p = 0; p < 50; p++) {
+const pacientes = []
+for(let p = 0; p < 10; p++) {
   pacientes.push({
     id: p,
     nome: `Fulano${p}`,
@@ -8,6 +8,11 @@ for(let p = 0; p < 50; p++) {
   })
 }
 
+const { DynamoDB } = require('aws-sdk');
+const AWS = require('aws-sdk')
+const params = {
+  TableName: 'PACIENTES'
+}
 function acharPaciente(id) {
   return pacientes.find(({id : _id}) => _id === id)
 }
@@ -15,34 +20,61 @@ function acharPaciente(id) {
 function filtrarMaiorIdade(idade) {
   return pacientes.filter(({idade: _idade}) => _idade > idade)
 }
+const dynamoDB = new AWS.DynamoDB.DocumentClient()
+
+
 module.exports.listarPacientes = async (event) => {
   // const {idade} = event
   // const paciente = filtrarMaiorIdade(idade);
-  return {
-    statusCode: 200, 
-    body: JSON.stringify(
-      {
-       pacientes
-      },
-      null,
-      2
-    ),
-  };
+  try {
+    const data = await dynamoDB.scan(params).promise()
+    return {
+      statusCode:200,
+      body: JSON.stringify(data.Items)
+    }
+  } catch(err) {
+    console.log("Error", err.message)
+    return {
+      statusCode: err.statusCode ? err.statusCode : 500,
+      body: JSON.stringify({
+        error: err.name ? err.name : "Exception",
+        message: err.message ? err.message : "Unknown error"
+      })
+    }
+  }
 };
 
 module.exports.obterPaciente = async (event) => {
-  const {id} = event.pathParameters;
-  const paciente = pacientes.find(({id: _id}) => id == _id);
-  return {
-    statusCode: paciente ? 200 : 404, 
-    body: JSON.stringify(
-      {
-        response: paciente ? {paciente} : {error: "Nenhum paciente encontrado" }
+  try {
+    const { id } = event.pathParameters;
+    console.log(event)
+    const data = await dynamoDB.get({
+      ...params,
+      Key: {
+        paciente_id: id
+      }
+    }).promise()
 
-      },
-      null,
-      2
-    ),
-  };
+    if(!data.Item) {
+      return {
+        statusCode: 404,
+        body: JSON.stringify({error: 'Paciente não existe'}, null, 2)
+      }
+    }
+    const paciente = data.Item;
+
+    return {
+      statusCode: 200,
+      body: JSON.stringify(paciente, null, 2)
+    }
+  } catch(err) {
+    return {
+      statusCode: err.statusCode ? err.statusCode : 500,
+      body: JSON.stringify({
+        error: err.name ? err.name : 'Exception',
+        message: err.message ? err.message : 'Unknown error'
+      })
+    }
+  }
 }
 
